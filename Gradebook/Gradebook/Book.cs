a0ebc0425f6c8cd4f1e1ff55786b0b68b2dd56fa
Mxrhodes <rhodesmateo@gmail.com>
2019-07-21 19:56:23 -0400

    Gradebook setup

---
+++ Gradebook/Gradebook/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gradebook
+{
+    class Book
+    {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowAllGrades()
+        {
+            foreach(double grade in grades)
+            {
+                Console.WriteLine($"-- {grade}");
+            }
+        }
+        public void ShowStatistics()
+        {
+            double result = 0.0;
+            double lowestGrade = double.MaxValue;
+            double highestGrade = double.MinValue;
+
+            foreach (double number in grades)
+            {
+                lowestGrade = Math.Min(number, lowestGrade);
+                highestGrade = Math.Max(number, highestGrade);
+                result += number;
+            }
+            double gradeAverage = result /= grades.Count;
+            Console.WriteLine($"Lowest Grade: {lowestGrade}, Highest Grade: {highestGrade}, Average grade: {gradeAverage:N2}");
+
+        }
+
+
+    }
+}
---
+++ Gradebook/Gradebook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var history101Grades = new Book("Charlotte's class");
+            history101Grades.AddGrade(89.1);
+            history101Grades.AddGrade(98.1);
+            history101Grades.AddGrade(73.1);
+            history101Grades.ShowAllGrades();
+            history101Grades.ShowStatistics();
+            
+        }
+    }
+}
